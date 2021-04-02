a766fcf3fe4604e08f6fc3642ba0d8576f0fa0f9
Konekotyan <Pistolero01@mail.ru>
2021-04-02 22:16:06 +0300

    hibernate

--- src/main/java/ru/mirea/estore/EstoreApplication.java
+++ src/main/java/ru/mirea/estore/EstoreApplication.java
@@ -2,8 +2,18 @@ package ru.mirea.estore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
 
 @SpringBootApplication
+
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class })
+
 public class EstoreApplication {
 
 	public static void main(String[] args) {
