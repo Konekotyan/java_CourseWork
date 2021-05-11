3f37aa1741cdbf8d8d3ffe32bc3875b02cce037b
Konekotyan <Pistolero01@mail.ru>
2021-05-11 13:40:07 +0300

    adding utils

---
+++ src/main/java/ru/mirea/estore/utils/Utils.java
@@ -0,0 +1,36 @@
+package ru.mirea.estore.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+import ru.mirea.estore.model.StoreInfo;
+
+public class Utils {
+
+    // Products, stored in Session.
+    public static StoreInfo getStoreInSession(HttpServletRequest request) {
+
+        StoreInfo storeInfo = (StoreInfo) request.getSession().getAttribute("myStore");
+
+
+        if (storeInfo == null) {
+            storeInfo = new StoreInfo();
+
+            request.getSession().setAttribute("myStore", storeInfo);
+        }
+
+        return storeInfo;
+    }
+
+    public static void removeStoreInSession(HttpServletRequest request) {
+        request.getSession().removeAttribute("myStore");
+    }
+
+    public static void storeLastOrderedStoreInSession(HttpServletRequest request, StoreInfo storeInfo) {
+        request.getSession().setAttribute("lastOrderedStore", storeInfo);
+    }
+
+    public static StoreInfo getLastOrderedStoreInSession(HttpServletRequest request) {
+        return (StoreInfo) request.getSession().getAttribute("lastOrderedStore");
+    }
+
+}
